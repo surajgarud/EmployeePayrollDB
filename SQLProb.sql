19bf062841c893dbe838882d154acbdd19d0b1c1
Suraj Garud <surajgarud95@gmail.com>
2022-02-20 15:36:26 +0530

    Create Employee Payroll Table

--- SQLProb.sql
+++ SQLProb.sql
@@ -1,5 +1,15 @@
         UC1
 
-
+create database payroll_service
 use payroll_service
 select * from sys.databases
+
+      UC2
+
+create table employee_payroll1
+(
+id int primary key identity,
+name varchar(50) not null unique,
+salary money not null,
+start date not null
+)
