7ca777ec18521b59f55f11906135bc8b17dd01ea
Suraj Garud <surajgarud95@gmail.com>
2022-02-20 16:03:30 +0530

    Retrieve data For particular Employee

--- SQLProb.sql
+++ SQLProb.sql
@@ -23,3 +23,11 @@ insert into employee_payroll1 values
 ('ramesh',16345.90,'2019-02-07'),
 ('gauri',14312.56,'2022-02-16'),
 ('rakesh',9000.0,'2021-11-28')
+
+		UC5
+
+select salary from employee_payroll1 where name='ramesh'
+
+select * from employee_payroll1 where start between '2018-01-01' and '2022-01-01'
+
+select cast(getdate() as varchar(20))
